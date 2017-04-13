11034796ea77fc4206851d3d6c43235ff0484123
MarkShmitt <shmitt.m@yandex.ua>
2017-04-13 22:13:17 +0300

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -5,7 +5,6 @@ fetch('https://github.com/MarkShmitt/Project/blob/master/articles.json', {mode:'
      var scavenger = document.getElementsByClassName('scavenger')[0];
      var section = document.createElement('section');
      section.className = "alert";
-
-    section.innerHTML = e.header_name + '<p>' +e.author+ '<p>' +e.contents+ '<p>' +e.created;
-    scavenger.appendChild(section); })
+     section.innerHTML = e.header_name + '<p>' +e.author+ '<p>' +e.contents+ '<p>' +e.created;
+     scavenger.appendChild(section); })
     });
